f926478203da2dacad9409951f11f512dadc8e8c
jay <log901108@gmail.com>
2019-04-10 00:40:45 +0900

    Api

--- client/src/App.js
+++ client/src/App.js
@@ -18,35 +18,26 @@ const styles = theme => ({
     table:{
         minwidth: 1080,
     }
-})
-
-
-const customers = [{
-    'id': 1,
-    'image' : 'https://placeimg.com/64/64/any',
-    'name' : 'hong',
-    'birthday': '910108',
-    'gender' : 'M',
-    'job' : 'Univ'
-},{
-  'id': 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : 'gil',
-  'birthday': '910108',
-  'gender' : 'M',
-  'job' : 'Univ'
-},
-{
-  'id': 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : 'dong',
-  'birthday': '910108',
-  'gender' : 'M',
-  'job' : 'Univ'
-}
-]
+});
 
 class App extends Component {
+
+  state={
+    customers:""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err=>console.log(err));
+  }
+
+callApi = async() =>{
+  const response = await fetch('/api/customers');
+  const body = await response.json();
+  return body;
+}
+
   render() {
 
     const {classes} = this.props;
@@ -65,7 +56,7 @@ class App extends Component {
            </TableHead>
             <TableBody>
               {
-                customers.map(c => {
+                this.state.customers ? this.state.customers.map(c => {
                   return(
                     <Customer
                         key={c.id}
@@ -77,7 +68,7 @@ class App extends Component {
                         job={c.job}
                       />
                   );
-                })
+                }) : ""
               }
         </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req,res)=>{
-    res.send({message: 'Hello Express'});
+app.get('/api/customers', (req,res)=>{
+    res.send([
+        {
+            'id': 1,
+            'image' : 'https://placeimg.com/64/64/any',
+            'name' : 'hong',
+            'birthday': '910108',
+            'gender' : 'M',
+            'job' : 'Univ'
+        },{
+          'id': 2,
+          'image' : 'https://placeimg.com/64/64/2',
+          'name' : 'gil',
+          'birthday': '910108',
+          'gender' : 'M',
+          'job' : 'Univ'
+        },
+        {
+          'id': 3,
+          'image' : 'https://placeimg.com/64/64/3',
+          'name' : 'dong',
+          'birthday': '910108',
+          'gender' : 'M',
+          'job' : 'Univ'
+        } 
+    ]);
 });
 
-app.listen(port,()=> console.log(`Listening on port${port}`));
+app.listen(port,()=> console.log(`Listening on port ${port}`));
