cc96336ca289a2d38891b6e55c29fac1a99aa58b
jay <log901108@gmail.com>
2019-04-10 13:28:41 +0900

    Progress bar

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -17,17 +18,23 @@ const styles = theme => ({
     },
     table:{
         minwidth: 1080,
+    },
+    progress:{
+      margin: theme.spacing.unit *2
     }
 });
 
 class App extends Component {
 
   state={
-    customers:""
+    customers:"",
+    completed: 0
   }
 
-  componentDidMount(){
-    this.callApi()
+componentDidMount(){
+  this.timer = setInterval(this.progress, 20); //progress bar
+
+  this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err=>console.log(err));
   }
@@ -36,6 +43,11 @@ callApi = async() =>{
   const response = await fetch('/api/customers');
   const body = await response.json();
   return body;
+}
+
+progress = () =>{
+  const { completed } = this.state;
+  this.setState({completed: completed>= 100 ? 0 : completed +1 });
 }
 
   render() {
@@ -68,7 +80,12 @@ callApi = async() =>{
                         job={c.job}
                       />
                   );
-                }) : ""
+                }) : 
+                <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                  </TableCell>
+                </TableRow>
               }
         </TableBody>
         </Table>
