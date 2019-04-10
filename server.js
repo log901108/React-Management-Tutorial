670137dd0698222a42e896a7c322f32c0a212510
jay <log901108@gmail.com>
2019-04-10 14:47:44 +0900

    Create Customer Database table & Node.js database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,32 +8,28 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req,res)=>{
-    res.send([
-        {
-            'id': 1,
-            'image' : 'https://placeimg.com/64/64/any',
-            'name' : 'hong',
-            'birthday': '910108',
-            'gender' : 'M',
-            'job' : 'Univ'
-        },{
-          'id': 2,
-          'image' : 'https://placeimg.com/64/64/2',
-          'name' : 'gil',
-          'birthday': '910108',
-          'gender' : 'M',
-          'job' : 'Univ'
-        },
-        {
-          'id': 3,
-          'image' : 'https://placeimg.com/64/64/3',
-          'name' : 'dong',
-          'birthday': '910108',
-          'gender' : 'M',
-          'job' : 'Univ'
-        } 
-    ]);
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err, rows, fields)=>{
+        res.send(rows);
+        
+      }
+    );
 });
 
 app.listen(port,()=> console.log(`Listening on port ${port}`));
